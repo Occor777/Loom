0f8e8c33920d6612c97468949225a05f3534d52c
Occor777 <cbravoco@gmail.com>
2024-08-05 20:24:45 -0400

    Added Two test Routes

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express_application = express();
+const my_port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+// Home route
+express_application.get('/', (req, res) => {
+    res.send('Welcome to the TTRPG Character Sheet App!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+// Users route
+express_application.get('/users', (req, res) => {
+    // Placeholder response for now
+    res.send('List of users will be here');
+});
+
+express_application.post('/users', (req, res) => {
+    // Placeholder response for now
+    res.send('User created');
+});
+
+express_application.listen(my_port, () => {
+    console.log(`Server is running on http://localhost:${my_port}`);
 });
